40f8bf9dff2b17f43ba60649c103d89fc1b4f1c8
Venu Gande <venu.gandhe@gmail.com>
2020-04-19 18:46:42 +0200

    map operator: used to update the price

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap } from 'rxjs/operators';
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { Product } from './product';
 import { Supplier } from '../suppliers/supplier';
@@ -16,6 +16,12 @@ export class ProductService {
   private suppliersUrl = this.supplierService.suppliersUrl;
 
   products$: Observable<Product[]> = this.http.get<Product[]>(this.productsUrl).pipe(
+    map(products => products.map(product =>
+      ({
+        ...product,
+        price: product.price * 1.5,
+        searchKey: [product.productName]
+      }) as Product)),
     tap(data => console.log('Products: ', JSON.stringify(data))),
     catchError(this.handleError)
   );
